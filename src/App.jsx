75939eca8ab74d2d417d9757891d99a28462f0a7
Julian <julianser@hotmail.com.ar>
2023-09-05 17:33:17 -0300

    Se realizo la forma de la calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
-
+import { Container } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Calculator from "./components/calculator/Calculator"
+import "./App.css"
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Container>
+      <Calculator></Calculator>
+    </Container>
   )
 }
 
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Calculator = () => {
+    return (
+        <Container>
+            <div className='calculator'>
+                
+            </div>
+        </Container>
+    );
+};
+
+export default Calculator;
