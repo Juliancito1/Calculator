0485315b7d5522badb5e28dd3c2a99ed53b381eb
Julian <julianser@hotmail.com.ar>
2023-09-05 17:46:19 -0300

    Se agrego el component Result en el cual se mostrara el resultado de la calculadora

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import Result from './Result';
 
 const Calculator = () => {
     return (
         <Container>
             <div className='calculator'>
-                
+                <Row>
+                    <Col lg={9}>
+                    <h3>Calc</h3>
+                    </Col>
+                    <Col lg={3}>
+                    <h4>Theme</h4>
+                    </Col>
+                </Row>
+                <Result></Result>
             </div>
         </Container>
     );
---
+++ src/components/calculator/Result.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Result = () => {
+    return (
+        <section>
+            <aside className='result mt-lg-4'>
+                        <h2 className='text-end me-lg-3 mt-lg-4'>28</h2>
+            </aside>
+        </section>
+    );
+};
+
+export default Result;
